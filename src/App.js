a4d76cec987e58ec4e1be1af30fb3e28858f5a93
sarveshatale-stack <71748819+sarveshatale-stack@users.noreply.github.com>
2020-09-23 19:46:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h2>Welcome To React Demo</h2>
+      <div>Welcome To React</div>
+    </div>
+  );
+}
